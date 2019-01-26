d114a6227a3d5ae369d39cc95c680d7cb59b5b05
Sergey Savchuk <sleepingdog.dev@gmail.com>
2019-01-26 14:25:44 +0300

    Ray spawner

---
+++ Assets/Scenes/GameScene/Scripts/View/RaySpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private float Cooldown = 1;
+    [SerializeField]
+    private float SpawnRadius;
+    [SerializeField]
+    private RadiusSpawner Spawner;
+    [SerializeField]
+    private GameObject Ray;
+
+    private float timer;
+    
+    void Start()
+    {
+        timer = 0;
+    }
+    
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= Cooldown)
+        {
+            timer = 0;
+            Spawner.Radius = SpawnRadius;
+            Spawner.Spawn(Ray);
+        }
+    }
+}

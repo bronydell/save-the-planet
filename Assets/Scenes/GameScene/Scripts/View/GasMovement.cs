68a4fb7a587da426336e9a8fe0a3a2d7e6b3d33d
Sergey Savchuk <sleepingdog.dev@gmail.com>
2019-01-26 15:48:59 +0300

    Gas generaton

---
+++ Assets/Scenes/GameScene/Scripts/View/GasMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Scenes.GameScene.Scripts.View
+{
+    public class GasMovement : ProjectileMovement
+    {
+        private void FixedUpdate()
+        {
+            Rb.velocity = transform.up * speed * Time.fixedDeltaTime * -1;
+        }
+    }
+}

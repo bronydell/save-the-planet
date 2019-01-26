a620412d9fb20f9f2cbe45172cb3140ba8acd028
Rostislav Nikishin <littleponyapps@gmail.com>
2019-01-26 13:35:56 +0300

    Added Earth and radius spawner

---
+++ Assets/Scenes/GameScene/Scripts/View/RadiusSpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class RadiusSpawner : MonoBehaviour
+{
+    public float Radius = 5;
+
+    public void Spawn(GameObject prefab)
+    {
+        var pos = RandomPointAtRadius() * Radius;
+        Instantiate(prefab, pos, Quaternion.identity);
+    }
+
+    private Vector2 RandomPointAtRadius()
+    {
+        return Random.insideUnitCircle.normalized;
+    }
+}
